67916afd396992e3efb17044f8ac98df694bbc13
Jimi Damon <jdamon@accesio.com>
2016-09-12 19:54:43 -0700

    ENH: new cmd ...reduced

--- cmd.rb
+++ cmd.rb
@@ -5,20 +5,29 @@ gem 'chronic'
 require 'byebug'
 
 class GlobalCommand
-  attr_reader :message
-  attr_reader :name
-  attr_reader :logger
-  attr_reader :level
-  
+  @@attributes = {
+    :message => "Running",
+    :name => lambda { |slf| slf.to_s.downcase },
+    :logger => 1,
+    :level => nil
+  }
+
   def self.[]( *args, raw )
+    if args.length % 2 != 0 
+      raise "ARGS length must be multiple of 2 "
+    end
     tmpargs = Hash[*args]
     @args = tmpargs
-    if ! tmpargs.has_key?(:name)
-      @name = self.to_s.downcase
-    end
-    if ! tmpargs.has_key?(:message)
-      @message = "Running "
-    end
+    @@attributes.each { |i|
+      if tmpargs.has_key?(i)
+        if attributes[i].lambda? 
+          self.class_variable_set("@@#{i}", attributes[i].call( self ) )
+        else
+          self.class_variable_set("@@#{i}", tmpargs[i] )
+        end
+      end
+    }
+
     @raw = raw
     self.runit( @raw )
   end
@@ -27,9 +36,6 @@ class GlobalCommand
     @cmd = cmd
   end
 
-  # need to make this generic 
-  # and overrideable with 
-  # meta-programming
   def self.runit(hargs)
     self.new("#{self.to_s.downcase} #{hargs}")
   end
